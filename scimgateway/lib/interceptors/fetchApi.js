fc9c6f17f77b41dee8908eaf222be6272cbccf70
Samuel <samuel-vianna@hotmail.com>
2023-06-27 10:46:58 -0300

    feat: auth and mapping for api interceptor

--- scimgateway/lib/interceptors/fetchApi.js
+++ scimgateway/lib/interceptors/fetchApi.js
@@ -1,5 +1,31 @@
 const { requests } = require("./requests");
 
+function formatAuth(auth) {
+  switch (auth.type) {
+    case "bearer":
+      return "Bearer " + auth.token;
+    case "basic":
+      return "Basic " + btoa(`${auth.username}:${auth.password}`);
+    default:
+      return "";
+  }
+}
+
+// replacing {{text}} variables in string to body values
+function formatURL(body, url) {
+  const replaceKey = (match, key) => {
+    // Verify if key exists in body
+    if (body[key]) {
+      return body[key];
+    }
+
+    // throw an error if key does not exist
+    throw new Error(`Missing required field ${key} in body`);
+  };
+
+  return url.replace(/{{(.*?)}}/g, replaceKey);
+}
+
 async function fetchApi(ctx, next) {
   const results = await Promise.all(
     requests.map(async (request) => {
@@ -8,16 +34,26 @@ async function fetchApi(ctx, next) {
       }
 
       try {
-        const response = await fetch(request.url, { method: request.method });
+        let formattedURL = formatURL(ctx.request.body, request.url);
+        const response = await fetch(formattedURL, {
+          method: request.method,
+          headers: { Authorization: formatAuth(request.auth) },
+        });
         const jsonData = await response.json();
-        ctx.request.body = { ...ctx.request.body, ...jsonData };
+
+        let responseAttrs = {};
+        request.mapping.forEach((item) => {
+          responseAttrs[item.mapTo] = jsonData[item.name];
+        });
+
+        ctx.request.body = { ...ctx.request.body, ...responseAttrs };
         return true;
       } catch (error) {
         ctx.status = 400;
         ctx.body = {
           message: "Error while fetching interceptor api",
           url: request.url,
-          error: error.message
+          error: error.message,
         };
         return ctx;
       }
@@ -28,4 +64,3 @@ async function fetchApi(ctx, next) {
 }
 
 module.exports = { fetchApi };
-
